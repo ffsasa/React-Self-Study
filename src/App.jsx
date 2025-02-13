644246f90e38d6666bc804160f008a157c20358e
vun30 <hoanvu3012@gmail.com>
2025-02-13 11:05:41 +0700

    study how to use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./components/todo/todo.css";
 import ToDoNew from "./components/todo/TodoNew";
 import TodoStuff from "./components/todo/TodoStuff";
-import reactLogo from "./assets/react.svg"
+import reactLogo from "./assets/react.svg";
 function App() {
   const vu = "vu dep trai";
   const data = {
-    address:"mangyang",
-    country:"vietnam"
-  }
+    address: "mangyang",
+    country: "vietnam",
+  };
+  const addNewTodo = (vu) => {
+    alert(`call me ${vu}`);
+  };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo list</div>
-      <ToDoNew />
-      <TodoStuff name = {vu} data = {data}/>
+      <ToDoNew addNewTodo={addNewTodo} name={vu} />
+      <TodoStuff name={vu} data={data} />
       <div className="todo-image">
-        <img className="logo" src={reactLogo}/>
+        <img className="logo" src={reactLogo} />
       </div>
     </div>
   );
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,13 @@
-const ToDoNew = () => {
-    return (
-      <div>
-        <input type="text" />
-        <button>Add</button>
-      </div>
-    );
-}
+const ToDoNew = (props) => {
+  console.log(props);
+  const { addNewTodo, name } = props;
+  addNewTodo(name);
+  return (
+    <div>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  );
+};
 
 export default ToDoNew;
